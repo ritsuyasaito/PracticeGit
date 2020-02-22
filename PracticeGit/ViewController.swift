0a6316efdb8340b50cc32500a1a225b5d5b45f26
【ritsuyasaito】 <ritsuya.basketball@gmail.com>
2020-02-22 15:42:18 +0900

    Initial Commit

---
+++ PracticeGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeGit
+//
+//  Created by 齋藤律哉 on 2020/02/22.
+//  Copyright © 2020 ritsuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
