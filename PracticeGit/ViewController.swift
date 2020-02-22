e5a3e97266d9fff1f888ecfa3296dfc7dd8b3f00
【ritsuyasaito】 <ritsuya.basketball@gmail.com>
2020-02-22 16:43:02 +0900

    テスト用で、ViewControllerにメソッドを追加

--- PracticeGit/ViewController.swift
+++ PracticeGit/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func test() {
+        print("a")
+    }
 
 
 }
